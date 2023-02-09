f747efe160b11e4a3d325e92568c01b7d2bea9aa
alvarito101093 <alvaro101093@gmail.com>
2023-02-09 18:56:32 +0100

    lucha a muerte con el sessionstorage....va ganando el de momento

--- js/playerscreen.js
+++ js/playerscreen.js
@@ -1,1 +1,37 @@
-const botonJugar = document.getElementById("botonJugar").addEventListener("click");
+// const botonJugar = document.getElementById("botonJugar").addEventListener("click");
+
+
+
+// function storagePlayers () {
+//     const player1InputName = document.getElementById("player1name");
+//     let player1Name = player1InputName.ariaValueMax;
+//     sessionStorage.setItem("player1name",player1Name);
+
+
+//     const player2InputName = document.getElementById("player2name");
+//     let player2Name = player2InputName.ariaValueMax;
+//     sessionStorage.setItem("player2name",player2Name);
+
+
+// if (player1Name == "" || player2Name == "") {
+//     alert("Tienes que introducir ambos nombre para poder comenzar");
+// } else {
+//     windows.location.href = "../pages/tablero.html"
+// }
+// }
+
+// windows.open("../pages/tablero.html","_selft");
+
+let player1Input = document.querySelector("player1name");
+let player2Input = document.querySelector("player2name");
+
+let startBtn = document.getElementById("botonJugar");
+
+startBtn.addEventListener("click", storagePlayersNames);
+
+const storagePlayersNames = () => {
+    let player1Name = player1Input.value;
+    let player2Name = player2Input.value;
+    sessionStorage.setItem('player1-name',player1Name);
+    sessionStorage.setItem('player2-name',player2Name);
+}
--- js/tablero.js
+++ js/tablero.js
@@ -1,6 +1,47 @@
-let jugador1 = "X";
-let jugador2 = "O";
+
+
+// let ficha1 = 3;
+// let ficha2 = 3;
+// let turno = true;
+
+// let tablero = Array.from(document.getElementsByClassName("cellDesign"));
+// let tableroLogico = ["","","","","","","","",""];
+
+// const jugador1nombre = document.getElementById("player1name");
+// const jugador2nombre = document.getElementById("player2name");
+
+
+// SESSIONSTORAGE
+
+// jugador1nombre.innerHTML = sessionStorage.getItem("player1name");
+// jugador2nombre.innerHTML = sessionStorage.getItem("player2name");
+
+// document.getElementById('text-screen').innerHTML = `Es el turno de ${jugador1.nombre}`
 
 
 
+// windows.open("../pages/tablero.html","_selft");
 
+
+
+// JUGADORES
+
+// class Player {
+//     constructor(nombre,mark,oppositeMark) {
+//         this.nombre = nombre.innerHTML,
+//         this.mark = mark,
+//         this.oppositeMark = oppositeMark
+//     };
+// };
+
+// let player1 = new Player(jugador1nombre, 'X','O');
+// let player2 = new Player(jugador2nombre, 'X','O');
+
+
+
+let nameP1Placeholder = document.querySelector(".player1-name-placeholder");
+let nameP2Placeholder = document.querySelector(".player2-name-placeholder");
+
+
+let player1 = sessionStorage.getItem('player1-name');
+let player2 = sessionStorage.getItem('player2-name');
