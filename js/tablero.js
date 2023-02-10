f28d83b9695e8c18c38f11662f6ac1cc91a9eb0a
alvarito101093 <alvaro101093@gmail.com>
2023-02-10 14:51:12 +0100

    Fichas se pueden poner y limite 6 falta recoger y victoria...

--- js/tablero.js
+++ js/tablero.js
@@ -5,3 +5,23 @@ let datosSesion = (sessionStorage.getItem("playersInfo"));
 
 player1.innetHTML = `${datosSesion.player1}`;
 player2.innetHTML = `${datosSesion.player2}`;
+
+
+let tablero = Array.from(document.getElementsByClassName("cellDesign"));
+let turno = true;
+let fichaP1 = 3;
+let fichaP2 = 3;
+let miTablero = ["","","","","","","","",""];
+
+tablero.map(
+    (celda) => {
+        celda.addEventListener('click', ()=> {
+            if((celda.innerHTML === "") && (fichaP1 > 0 || fichaP2 > 0)){
+                celda.innerHTML = (turno) ? "X" : "O";
+                (turno) ? fichaP1-- : fichaP2--;
+                miTablero[celda.id] = (turno) ? "X" : "O";
+                turno = !turno;
+            }
+        })
+    }
+)
