cea0ec2bc5328f43ae74f0349cfd1888b07770e9
alvarito101093 <alvaro101093@gmail.com>
2023-02-16 10:25:01 +0100

    limpieza de códigos js

--- js/tablero.js
+++ js/tablero.js
@@ -1,19 +1,15 @@
 
-//sessionStorage
-const nombrejugador = () => {
-    document.getElementById("Player1").innerHTML = sessionStorage.getItem("name1")
-    document.getElementById("Player2").innerHTML = sessionStorage.getItem("name2")
-}
+    //Traigo los nombres por sessionStorage
+document.getElementById("Player1").innerHTML = sessionStorage.getItem("name1")
+document.getElementById("Player2").innerHTML = sessionStorage.getItem("name2")
 
-nombrejugador ();
-
-let tablero = Array.from(document.getElementsByClassName("cellDesign"));
+    //Declaro todas las variables del juego
 let turno = true;
 let fichaP1 = 3;
 let fichaP2 = 3;
+let tablero = Array.from(document.getElementsByClassName("cellDesign"));
 let miTablero = ["","","","","","","","",""];
 let jugador1 = "X"
-
 let victoria = [
     [0, 1, 2],
     [3, 4, 5],
@@ -25,9 +21,11 @@ let victoria = [
     [2, 4, 6],
 ];
 
+    //Esto es la parte que contiene toda la partida
 tablero.map(
     (celda) => {
         celda.addEventListener('click', ()=> {
+            //Cambiar de turno cuando uno pone ficha
             if((celda.innerHTML === "") && (fichaP1 > 0 || fichaP2 > 0)){
                 celda.innerHTML = (turno) ? "X" : "O";
                 (turno) ? fichaP1-- : fichaP2--;
@@ -51,49 +49,30 @@ tablero.map(
     }
 )
 
-const result = [];
-
-// let miTablero = ["","","","","","","","",""];
-// let victoria = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6],
-// ];
-
-let ganar = (debate) => {
+let ganador = [];
+    //Función que gana la partida al ser las 3 columnas iguales
+let ganar = () => {
     for (let i = 0; i < victoria.length; i++) {
         let win = victoria[i]
-        let win1 = tablero[win[0]].innerHTML
-        let win2 = tablero[win[1]].innerHTML
-        let win3 = tablero[win[2]].innerHTML
-        if (win1 === "" || win2 === "" || win3 === "") {
+        let columna1 = tablero[win[0]].innerHTML
+        let columna2 = tablero[win[1]].innerHTML
+        let columna3 = tablero[win[2]].innerHTML
+        if (columna1 === "" || columna2 === "" || columna3 === "") {
             continue;
         }
-        if (win1 === win2 && win2 === win3) {
-            result.push(win1).innerHTML
-            // result.push("turno")
-            console.log(result)
-            return (debate, storageWinner())
+        if (columna1 === columna2 && columna2 === columna3) {
+            //Envía el valir de "X" o "O" dentro del ganador
+            ganador.push(columna1).innerHTML
+            return (storageWinner())
         }
     }
 };
-
+    //Función que envía el nombre del ganador a "winner"
 let storageWinner = () => {
-    // console.log(jugador1)
-    console.log("el resultado de result es " , result[0])
-    if (result[0] === "X") {
-        // sessionStorage.setItem('winner', JSON.stringify(player1.name));
+    if (ganador[0] === "X") {
         sessionStorage.setItem('winner', sessionStorage.getItem("name1")); 
-        console.log("ha ganado el usuario1")
     } else {
         sessionStorage.setItem('winner', sessionStorage.getItem("name2"));
-        console.log("ha ganado el usuario2")
     }
     window.location = "../pages/victoria.html"
-    // console.log(`ha ganado el jugador ${'winner'}`)
 };
--- js/victoria.js
+++ js/victoria.js
@@ -1,10 +1,8 @@
+    //Traigo al ganador de la partida
 let jugadores = sessionStorage.getItem("winner")
 
-// let muestrawin = document.getElementById("jugadores");
+    //Lo envio al Id "ganador" y lo muestro en pantalla
+document.getElementById("ganador").innerHTML = (JSON.stringify(jugadores))
 
-
-const nombreganador = () => {
-    document.getElementById("ganador").innerHTML = (JSON.stringify(jugadores))
-}
-
-nombreganador ();
+    //Limpiar el seasonStorage para volver a empezar
+sessionStorage.clear();
